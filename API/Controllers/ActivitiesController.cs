651813dc841ceda392ce266a272bf3c013cee5a3
Nikolay <nick.masson@yandex.ru>
2021-07-15 21:40:43 +0300

    Added CRUD operations with MediatR

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -20,13 +23,37 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            var activities = await Mediator.Send(new List.Query());
+            return Ok(activities);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            var result = await Mediator.Send(new Create.Command(activity));
+            return CreatedAtAction(nameof(GetActivity), new {Id = activity.Id}, activity);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            // Setting activity id in case the DTO doesn't contain it
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command(activity));            
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            await Mediator.Send(new Delete.Command(id));
+            return NoContent();
         }
     }
 }
